3ee04ce3f6785103baa40ab9a35ad389acbdfffb
Juliana Prado <97170379+Julianapw@users.noreply.github.com>
2025-03-12 14:48:28 -0300

    Delete aplicacao1 directory

--- aplicacao1/target/classes/clientes.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO cliente (nome, telefone, correntista, score_credito, saldo_cc) 
-VALUES 
-    ('Maria Silva', 11987654321, 1, 85.0, 5000.0),
-    ('João Santos', 11912345678, 0, 70.0, 2000.0);
-ALTER TABLE cliente MODIFY saldo_cc DECIMAL(15,2);
-
